0e78f2c2d9eb623196d194095b952d3e15e95c64
Nick Avtges <navtges3@gmail.com>
2025-01-27 11:35:14 -0500

    Set up Hero base class

--- Heroes/Hero.py
+++ Heroes/Hero.py
@@ -1,21 +1,30 @@
 class hero:
+    
+    #Base class for all heroes
     def __init__(self, name, health, damage):
         self.name = name
         self.health = health
         self.damage = damage
+        self.monstersSlain = 0
 
+    def __str__(self):
+        return self.name
+    
     def isAlive(self):
         if self.health > 0:
             return True
         else:
-            return False 
+            return False
     
+    #Attack the target
     def attack(self, target):
         print(self.name + " Attacks " + target.name + " dealing " + str(self.damage) + " damage.")
         target.takeDamage(self.damage)
-        if target.isAlive() != True:
-            print(self.name + " has slain " + target.name)
-
+        if not target.isAlive():
+            self.monstersSlain += 1
+            print(self.name + " has slain " + str(target) + "!")
+            
+    #Take damage from an attacker
     def takeDamage(self, damage):
         self.health = self.health - damage
-        print(self.name + " has " + str(self.health) + " remaining.")  
+        print(self.name + " has " + str(self.health) + " health remaining.")  
--- Monsters/MonsterBase.py
+++ Monsters/MonsterBase.py
@@ -1,22 +1,30 @@
 class monsterBase:
 
+    #Base class for all monsters
     def __init__(self, name, health, damage):
         self.name = name
         self.health = health
         self.damage = damage
 
+    def __str__(self):
+        return self.name
+    
     def isAlive(self):
         if self.health > 0:
             return True
         else:
             return False
 
+    #Attack the target
     def attack(self, target):
         print(self.name + " Attacks " + target.name + " dealing " + str(self.damage) + " damage.")
         target.takeDamage(self.damage)
         if target.isAlive() != True:
-            print(self.name + " has slain " + target.name)
+            print(self.name + " has slain " + str(target) + "!")
 
+    #Take damage from an attacker
     def takeDamage(self, damage):
         self.health = self.health - damage
-        print(self.name + " has " + str(self.health) + " remaining.")
+        if self.health < 0:
+            self.health = 0
+        print(self.name + " has " + str(self.health) + " health remaining.")
