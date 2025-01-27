86289bb87195b6cf4c00a8abe025489ff996e6b8
Nick Avtges <navtges3@gmail.com>
2025-01-27 09:52:47 -0500

    Adding more functions to Hero and Goblin classes

--- Hero.py
+++ Hero.py
@@ -1,5 +1,21 @@
 class hero:
-    def __init__(self, name, health, attack):
+    def __init__(self, name, health, damage):
         self.name = name
         self.health = health
-        self.attack = attack
+        self.damage = damage
+
+    def isAlive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False 
+    
+    def attack(self, target):
+        print(self.name + " Attacks " + target.name + " dealing " + str(self.damage) + " damage.")
+        target.takeDamage(self.damage)
+        if target.isAlive() != True:
+            print(self.name + " has slain " + target.name)
+
+    def takeDamage(self, damage):
+        self.health = self.health - damage
+        print(self.name + " has " + str(self.health) + " remaining.")  
