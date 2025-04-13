30c550e99eadc30f870975d6da50763892881f22
Nick Avtges <navtges3@gmail.com>
2025-04-13 14:14:14 -0400

    Refactor equipmentDictionary to include detailed weapon descriptions and additional weapon types

--- Items.py
+++ Items.py
@@ -26,10 +26,16 @@ class Armor(Item):
     def print_stats(self):
         print(self.name + ": " + self.description + " Block: " + str(self.block))
 
-equipmentDictionary = { "Dagger": Weapon("Dagger", "A sharp dagger", 3),
-                        "Sword": Weapon("Sword", "A sharp sword", 5), 
-                        "Axe": Weapon("Axe", "A sharp axe", 7), 
-                        "Longsword": Weapon("Longsword", "A sharp Longsword", 9)}
+equipmentDictionary = { "Daggers": Weapon("Daggers", "A rogue’s signature: fast, agile, and perfect for quick, lethal strikes", 3),
+                        "Rapier": Weapon("Rapier", "A slender, piercing sword that allows for swift, elegant combat", 5), 
+                        "Throwing Knives": Weapon("Throwing Knives", "Silent, deadly, and ideal for surprise attacks from a distance", 7), 
+                        "Hand Crossbow": Weapon("Hand Crossbow", "A compact ranged weapon, great for assassinations and quick escapes", 9),
+                        "Shadow Whip": Weapon("Shadow Whip", "A mystical whip that ensnares foes and delivers vicious strikes", 11),
+                        "Greatsword": Weapon("Greatsword", "A massive, two-handed blade that delivers devastating slashes and cleaves through enemies", 3),
+                        "Warhammer": Weapon("Warhammer", "A brutal, heavy weapon that crushes armor and bones with raw force", 5), 
+                        "Battleaxe": Weapon("Battleaxe", "A hefty axe, ideal for chopping through foes with powerful, sweeping strikes", 7), 
+                        "Halberd": Weapon("Halberd", "A polearm with a sharp axe blade and a spear tip, providing reach and versatility", 9),
+                        "Flaming Greataxe": Weapon("Flaming Greataxe", "A mystical whip that ensnares foes and delivers vicious strikes", 11)}
 
 protectionDictionary = {    "Leather": Armor("Leather", "A sturdy leather suit", 2),
                             "Chainmail": Armor("Chainmail", "A suit of chainmail", 4),
